60b7241a892e8995440c4333ef3e575cd55d5f49
cerberus9116 <98425275+cerberus9116@users.noreply.github.com>
2023-12-28 13:51:07 -0300

    Update int.sh

--- int.sh
+++ int.sh
@@ -2,6 +2,6 @@
 apt update 
 apt upgrade -y
 apt install curl -y
-curl -o sshplus https:/164.152.44.71/sshpluspro/$(uname -m)/sshplus
+curl -o sshplus https:/164.152.44.71/$(uname -m)/sshplus
 chmod +x sshplus
 ./sshplus
